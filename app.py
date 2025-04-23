9e8f2a3482b2e5777dd5acc7d152bd80e43e16e3
code-stepan <108977209+code-stepan@users.noreply.github.com>
2025-04-23 17:04:25 +0300

    test

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/ping', methods=['GET'])
+def ping():
+    return jsonify({"message": "pong"})
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return jsonify({"message": "Hello, World!"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
